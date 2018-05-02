18ff3b82595a4576a8f7acfffe394512b294c4b5
cyzu <chloeyukino@gmail.com>
2018-05-02 17:28:01 +0200

    Projet matrice : Correction script

--- Projet_matrice/Test_matrice/script_test.sh
+++ Projet_matrice/Test_matrice/script_test.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# 0 true, success
-# 1 false, error
 
 # Ce script compile le code source dans le dossier scr/ puis l'excécute avec n processus passé en paramètre.
 # Il compare les résultats obtenus avec ceux du dossier Resultat/ puis affiche True ou False
@@ -11,32 +9,35 @@
 
 CC="mpicc"
 OPTION="-std=c99 -g -Wall"
-OMP="-fopenmp"          # pour openmp (-fopenmp)
+OMP=""          # pour openmp (-fopenmp)
 MATH=""                 # -lm si vous utilisez math.h
 RUN="mpirun"
 
 # NE PAS Y TOUCHER
 file=src/*
 result=""
-exec=""
+exec=executable
 
 if [ $# -ne 1 ]; then
     echo "Usage : $0 numprocs"
     exit 1
 fi
-exec=${1%.*}
+
+echo $CC $OPTION $OMP -o $exec $file $MATH
 $CC $OPTION $OMP -o $exec $file $MATH
 
 for i in {1..11}; do
+    echo $RUN --oversubscribe -np $1 ./$exec Test/test${i}A.txt Test/test${i}B.txt
     $RUN --oversubscribe -np $1 ./$exec Test/test${i}A.txt Test/test${i}B.txt > tmp.txt
 
     diff Resultat/resultat${i}.txt tmp.txt
-    if [ $? ]; then
+    if [ $? = 0 ]; then
         result+="True;"
     else result+="False;"
     fi
 done
 
-rm tmp.txt
+rm tmp.txt $exec
+echo ""
 echo $result
 exit 0
